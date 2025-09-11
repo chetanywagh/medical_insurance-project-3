c9a22f252a73cd88f2993f3e9ed8b91c507c875a
Chetan wagh <150357946+chetanywagh@users.noreply.github.com>
2025-09-11 12:50:54 +0530

    Update App.py

--- App.py
+++ App.py
@@ -72,6 +72,13 @@ st.write(
     "Predict your expected medical insurance costs by filling in the details below 👇"
 )
 
+# Short & clear description
+st.write(
+    "**Predict your medical insurance cost instantly!** \n\n"
+    "Predict your expected medical insurance costs by filling in the details below 👇"
+)
+
+
 # User inputs in 2-column layout
 st.header("User Information")
 col1, col2 = st.columns(2)
